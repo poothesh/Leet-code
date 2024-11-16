8f100a3cd1a244ed7ebd02af7b3cfc4cff271430
Poothesh M <157009913+poothesh@users.noreply.github.com>
2024-11-17 00:40:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i:nums){
+            if(nums[i]>=0){
+                ans[i] = nums[i];
+            }
+        }
+        for()
+    }
+}
