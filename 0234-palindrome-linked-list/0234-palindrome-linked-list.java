783efc33ee90232ad23829f912ea58a9b27194ff
Poothesh M <157009913+poothesh@users.noreply.github.com>
2024-12-10 09:42:52 +0530

    Time: 3 ms (99.72%), Space: 69 MB (18.26%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,11 +10,11 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head;
         ListNode fast = head;
+        ListNode slow = head;
         ListNode prev = null;
-        while(fast != null && fast.next != null){
-            fast = fast.next.next;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
             ListNode temp = slow.next;
             slow.next = prev;
             prev = slow;
@@ -23,8 +23,8 @@ class Solution {
         if(fast!=null){
             slow = slow.next;
         }
-        while(prev!=null && slow != null){
-            if(prev.val!=slow.val){
+        while(prev!=null && slow!=null){
+            if(prev.val != slow.val){
                 return false;
             }
             prev = prev.next;
