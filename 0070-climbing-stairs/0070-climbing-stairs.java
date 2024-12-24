0b728126b7e30e53db6d98a23f1449cd8b4218b0
Poothesh M <pootheshmm@gmail.com>
2024-12-24 12:28:01 +0530

    Time: 0 ms (100%), Space: 40.9 MB (6.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,14 @@
-class Solution {
+public class Solution {
     public int climbStairs(int n) {
-        Map<Integer, Integer> memo = new HashMap<>();
-        return climbStairs(n, memo);
-    }
-    
-    private int climbStairs(int n, Map<Integer, Integer> memo) {
-        if (n == 0 || n == 1) {
+        if (n <= 1) {
             return 1;
         }
-        if (!memo.containsKey(n)) {
-            memo.put(n, climbStairs(n-1, memo) + climbStairs(n-2, memo));
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return memo.get(n);
+        return dp[n];
     }
 }
